95d0e58a92fcfc90250d1db34e77f4d4bc9776dd
Samuel-joseph05 <samuela05022004@gmail.com>
2025-11-02 21:11:13 +0530

    feat: Resposive

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,19 +1,24 @@
 function HomePage() {
   return (
     <header className="shadow-lg">
-      <div className="max-w-7xl mx-auto px-7 py-5 lg:flex items-center">
+      <div className="max-w-7xl mx-auto px-7 py-5 lg:flex items-center lg:px-7 lg:py-2 lg:pt-3 ">
         {/* Logo - left */}
         <div className="shrink-0  flex justify-center">
-          <img src="icon.png" alt="Logo" className="h-14 md:h-16 lg:h-[55px] w-auto mb-2 md:mb-4" />
+          <img
+            src="icon.png"
+            alt="Logo"
+            className="h-14 md:h-16 lg:h-[55px] w-auto mb-2 md:mb-4"
+          />
         </div>
 
         {/* Search group - pushed to right */}
-        <div className="ml-auto flex items-center  space-x-3 md:flex justify-center">
-        <input
-  type="text"
-  placeholder="Search experiences"
-  className="w-64 md:w-90  lg:w-[325px] py-2 px-3 md:py-3  md:px-6 rounded-sm bg-[#efefef] border border-[#efefef] focus:outline-none focus:ring-2 focus:ring-amber-300 placeholder:text-[#afafaf]"
-/>
+        <div className="ml-auto flex items-center  space-x-3 ">
+          <input
+            type="text"
+            placeholder="Search experiences"
+            className="w-64 md:w-90  lg:w-[325px] py-2 px-3 md:py-3  md:px-6 rounded-sm 
+  bg-[#efefef] border border-[#efefef] focus:outline-none focus:ring-2 focus:ring-amber-300 placeholder:text-[#afafaf] text-[#727272]"
+          />
 
           <button
             type="button"
