babc9c6e33562c6f8c72c434a306f890f95097f7
Samuel-joseph05 <samuela05022004@gmail.com>
2025-11-02 14:05:14 +0530

    feat: add search button and input logo to home page

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,29 @@
+function HomePage() {
+  return (
+    <header className="shadow-lg">
+      <div className="max-w-7xl mx-auto px-7 py-5 lg:flex items-center">
+        {/* Logo - left */}
+        <div className="shrink-0  flex justify-center">
+          <img src="icon.png" alt="Logo" className="h-14 md:h-16 lg:h-[55px] w-auto mb-2 md:mb-4" />
+        </div>
+
+        {/* Search group - pushed to right */}
+        <div className="ml-auto flex items-center  space-x-3 md:flex justify-center">
+        <input
+  type="text"
+  placeholder="Search experiences"
+  className="w-64 md:w-90  lg:w-[325px] py-2 px-3 md:py-3  md:px-6 rounded-sm bg-[#efefef] border border-[#efefef] focus:outline-none focus:ring-2 focus:ring-amber-300 placeholder:text-[#afafaf]"
+/>
+
+          <button
+            type="button"
+            className="bg-[#ffd643] rounded-lg px-5 py-2 md:py-3  md:px-6  font-medium hover:bg-[#ffc107] hover:cursor-pointer"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+export default HomePage;
